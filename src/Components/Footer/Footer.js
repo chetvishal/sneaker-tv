9733a946524da5c8fe77ffb55a3ee4eae74f3fb8
Vishal Singh <40429237+chetvishal@users.noreply.github.com>
2022-04-27 21:12:39 +0530

    Update Footer.js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,18 +4,18 @@ export const Footer = () => {
 
     return (
         <footer className={styles.footer}>
-            <h3 className={styles.footer__heading}>Made by Chet Vishal Tunju</h3>
+            <h3 className={styles.footer__heading}>Made by Vishal Singh</h3>
             <span className={styles.footer_social_link}>
                 <a href="https://github.com/chetvishal" className="nostyle" rel="noreferrer"target="_blank">
                     <i className="fab fa-github" styles={{color: "black"}}></i>
                 </a></span>
             <span className={styles.footer_social_link}>
-                <a href="https://www.linkedin.com/in/chet-vishal-tunju-1056b7123/" className="nostyle" rel="noreferrer"target="_blank">
+                <a href="https://www.linkedin.com/in/vishal-singh-1056b7123/" className="nostyle" rel="noreferrer"target="_blank">
                     <i className="fab fa-linkedin-in"></i>
                 </a>
             </span>
             <span className={styles.footer_social_link}>
-                <a href="https://twitter.com/chetvishal" className="nostyle" rel="noreferrer"target="_blank">
+                <a href="https://twitter.com/vshsngh" className="nostyle" rel="noreferrer"target="_blank">
                     <i className="fab fa-twitter"></i>
                 </a>
             </span>
